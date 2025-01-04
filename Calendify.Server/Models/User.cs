fcf6944d90da9b616095e70cdf40df6f37194b38
SvenvanMuiden <Svenvm2004@gmail.com>
2025-01-04 12:48:46 +0100

    mini update

--- Calendify.Server/Models/User.cs
+++ Calendify.Server/Models/User.cs
@@ -7,11 +7,12 @@ namespace Calendify.Server.Models
     {
         public int Id { get; set; } // Unique identifier for each user
 
-        public string FirstName { get; set; } // First name of the user
+        public required string FirstName { get; set; } // First name of the user
 
-        public string LastName { get; set; } // Last name of the user
+        public required string LastName { get; set; } // Last name of the user
 
-        public string Email { get; set; } // Email address of the user
+        public required string Email { get; set; } // Email address of the user
+        public required string Password { get; set; }
         public string Role { get; set; }
 
         public List<EventAttendanceModel> Attendances { get; set; } = new List<EventAttendanceModel>();
