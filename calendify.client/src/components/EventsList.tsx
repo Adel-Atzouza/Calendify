02bb777e102eabceff582cb0f0e3d9c8ae5a2e08
mark-175 <152170112+mark-175@users.noreply.github.com>
2025-01-11 18:02:00 +0100

    delte unused component

--- calendify.client/src/components/EventsList.tsx
+++ calendify.client/src/components/EventsList.tsx
@@ -14,43 +14,6 @@ import {
   Button,
 } from "@mui/material";
 
-export function EventListWithDetails({ events }: { events: EventModel[] }) {
-  return (
-    <div>
-      <ul>
-        {events.map((ev, index) => (
-          <li key={index} style={{ marginBottom: "1rem" }}>
-            <h3>{ev.title}</h3>
-            <p>
-              <strong>Description:</strong> {ev.description}
-            </p>
-            <p>
-              <strong>Date:</strong> {ev.date}
-            </p>
-            <p>
-              <strong>Time:</strong> {ev.startTime} - {ev.endTime}
-            </p>
-            <p>
-              <strong>Location:</strong> {ev.location}
-            </p>
-            <p>
-              <strong>Max Attendees:</strong> {ev.maxAttendees}
-            </p>
-            <p>
-              <strong>Category:</strong> {ev.category}
-            </p>
-            <p>
-              <strong>Admin Approval:</strong> {ev.adminApproval ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Attendances:</strong>
-            </p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
 export const EventList = ({ Events }: { Events: EventModel[] }) => {
   return (
     <div>
